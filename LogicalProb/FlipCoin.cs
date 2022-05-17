f8656275a9ca44a6f7f8310ee8c7a45c82ac6b47
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-05-17 19:59:08 +0530

    [jayesh]  Flipping coin

---
+++ LogicalProb/FlipCoin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    internal class FlipCoin
+    {
+        public int Head = 0;
+        public int Tail = 0;
+        public void Flip_Coin()
+        {
+            Console.WriteLine("Enter How many times do you want to flip a coin");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number <= 0)
+            {
+                Console.WriteLine("Please enter a correct positive number");
+            }
+            else
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    Random RandomNumber = new Random();
+                    float Random_Number = (float)RandomNumber.NextDouble();
+                    if (Random_Number < 0.5)
+                    {
+                        Head++;
+                        {
+                            Tail++;
+                        }
+                        // Console.WriteLine(+Random_Number);
+                    }
+
+                    else
+                        Console.WriteLine("Number of Heads occurred: " + Head);
+                    Console.WriteLine("Number of Tails occurred: " + Tail);
+                    Console.WriteLine("% Head occured:" + (float)(Head * 100 / number));
+                    Console.WriteLine("% Tail occured:" + (float)(Tail * 100 / number));
+                }
+            }
+        }
+    }
+}
---
+++ LogicalProb/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Please enter number as per options");
+            Console.WriteLine("1: For Flip a coin and find % of Head and Tail");
+            int check = Convert.ToInt32(Console.ReadLine());
+            switch (check)
+            {
+
+                case 1:
+                    FlipCoin FlippingCoin = new FlipCoin();
+                    FlippingCoin.Flip_Coin();
+                    break;
+                
+                default:
+                    Console.WriteLine("Please SELECT correct number ! ");
+                    break;
+
+            }
+
+
+        }
+    }
+}
+
